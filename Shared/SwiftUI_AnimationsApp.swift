642ec6a6b727d2088bf30dc77626dfb029817b26
Manas Vijaywargiya <manasdevslopes@gmail.com>
2021-04-24 20:22:31 +0530

    Initial Commit

---
+++ Shared/SwiftUI_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationsApp.swift
+//  Shared
+//
+//  Created by MANAS VIJAYWARGIYA on 24/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
